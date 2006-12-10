fe7b7857515ce7152c4c30566ce6133550ae4514
Peter Donald <peter@realityforge.org>
2006-12-10 04:23:59 +0000

    Add version constants and convert to 2-spaces

--- src/main/java/org/realityforge/fade/ClassFileFormat.java
+++ src/main/java/org/realityforge/fade/ClassFileFormat.java
@@ -1,50 +1,57 @@
 package org.realityforge.fade;
 
-/**
- * Interface containing constants defining class file format.
- */
+/** Interface containing constants defining class file format. */
 public interface ClassFileFormat
 {
-    ///Magic number at start of file
-    int MAGIC = 0xCAFEBABE;
-    /*
-      Access modifiers and what structures can apply to.
-      Note: Some of the modifiers have the same value but apply
-      to different structures. i.e.
+  ///Magic number at start of file
+  int MAGIC = 0xCAFEBABE;
 
-      Class.ACC_SUPER == Method.ACC_SYNCHRONIZED
-      Field.ACC_VOLATILE == Method.ACC_BRIDGE
-      Field.ACC_TRANSIENT == Method.ACC_VARARGS
-    */
-    int ACC_PUBLIC = 0x0001; //Class or Field or Method
-    int ACC_PRIVATE = 0x0002; //Class or Field or Method
-    int ACC_PROTECTED = 0x0004; //Class or Field or Method
-    int ACC_STATIC = 0x0008; //Field or Method
-    int ACC_FINAL = 0x0010; //Class or Field or Method
-    int ACC_SUPER = 0x0020; //Class
-    int ACC_SYNCHRONIZED = 0x0020; //Method (== ACC_SUPER)
-    int ACC_VOLATILE = 0x0040; //Field
-    int ACC_BRIDGE = 0x0040; //Method
-    int ACC_VARARGS = 0x0080; //Method
-    int ACC_TRANSIENT = 0x0080; //Field
-    int ACC_NATIVE = 0x0100; //Method
-    int ACC_INTERFACE = 0x0200; //Class
-    int ACC_ABSTRACT = 0x0400; //Class or Method
-    int ACC_STRICT = 0x0800; //Method
-    int ACC_SYNTHETIC = 0x1000; //Class or Field or Method
-    int ACC_ANNOTATION = 0x2000; //Class
-    int ACC_ENUM = 0x4000; //Class
+  /* Major version numbers for class file format. */
+  int MAJOR_VERSION_1 = 45;
+  int MAJOR_VERSION_2 = 46;
+  int MAJOR_VERSION_3 = 47;
+  int MAJOR_VERSION_4 = 48;
+  int MAJOR_VERSION_5 = 49;
+  int MAJOR_VERSION_6 = 50;
 
-    /* Types in the constant pool. */
-    int CONSTANT_Utf8 = 1;
-    int CONSTANT_Integer = 3;
-    int CONSTANT_Float = 4;
-    int CONSTANT_Long = 5;
-    int CONSTANT_Double = 6;
-    int CONSTANT_Class = 7;
-    int CONSTANT_String = 8;
-    int CONSTANT_Fieldref = 9;
-    int CONSTANT_Methodref = 10;
-    int CONSTANT_InterfaceMethodref = 11;
-    int CONSTANT_NameAndType = 12;
+  /*
+    Access modifiers and what structures can apply to.
+    Note: Some of the modifiers have the same value but apply
+    to different structures. i.e.
+
+    Class.ACC_SUPER == Method.ACC_SYNCHRONIZED
+    Field.ACC_VOLATILE == Method.ACC_BRIDGE
+    Field.ACC_TRANSIENT == Method.ACC_VARARGS
+  */
+  int ACC_PUBLIC = 0x0001; //Class or Field or Method
+  int ACC_PRIVATE = 0x0002; //Class or Field or Method
+  int ACC_PROTECTED = 0x0004; //Class or Field or Method
+  int ACC_STATIC = 0x0008; //Field or Method
+  int ACC_FINAL = 0x0010; //Class or Field or Method
+  int ACC_SUPER = 0x0020; //Class
+  int ACC_SYNCHRONIZED = 0x0020; //Method (== ACC_SUPER)
+  int ACC_VOLATILE = 0x0040; //Field
+  int ACC_BRIDGE = 0x0040; //Method
+  int ACC_VARARGS = 0x0080; //Method
+  int ACC_TRANSIENT = 0x0080; //Field
+  int ACC_NATIVE = 0x0100; //Method
+  int ACC_INTERFACE = 0x0200; //Class
+  int ACC_ABSTRACT = 0x0400; //Class or Method
+  int ACC_STRICT = 0x0800; //Method
+  int ACC_SYNTHETIC = 0x1000; //Class or Field or Method
+  int ACC_ANNOTATION = 0x2000; //Class
+  int ACC_ENUM = 0x4000; //Class
+
+  /* Types in the constant pool. */
+  int CONSTANT_Utf8 = 1;
+  int CONSTANT_Integer = 3;
+  int CONSTANT_Float = 4;
+  int CONSTANT_Long = 5;
+  int CONSTANT_Double = 6;
+  int CONSTANT_Class = 7;
+  int CONSTANT_String = 8;
+  int CONSTANT_Fieldref = 9;
+  int CONSTANT_Methodref = 10;
+  int CONSTANT_InterfaceMethodref = 11;
+  int CONSTANT_NameAndType = 12;
 }
