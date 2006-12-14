400b9521ced0e31fd44bae3f6ea698ba7954099d
Peter Donald <peter@realityforge.org>
2006-12-14 04:29:30 +0000

    Move parsing of UTF strings out of ConstantPool and into IOUtil

--- src/main/java/org/realityforge/fade/IOUtil.java
+++ src/main/java/org/realityforge/fade/IOUtil.java
@@ -100,4 +100,96 @@ class IOUtil
       offset + " when class file is only " + data.length + " bytes long.";
     throw new ClassFormatError( message );
   }
+
+  /**
+   * Parse a UTF string from data.
+   * Format specified in < a href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/ClassFile.doc.html">ClassFile
+   * Spec</a>. Don't really understand it just mechanically translated.
+   *
+   * @param data       the data.
+   * @param baseOffset the offset to start reading utf from.
+   * @param index      the constant pool index. if -1 then not parsed from constant pool. Used in reporting exception.
+   * @param count      the number of characters in string.
+   * @return the string.
+   */
+  static String parseUtfString( final byte[] data,
+                                final int baseOffset,
+                                final int index,
+                                final int count )
+  {
+    int offset = baseOffset;
+    int size = 0;
+
+    //We may over-allocate but this is probably faster
+    final char[] chars = new char[count];
+
+    final int end = count + offset;
+
+    while( offset < end )
+    {
+      final byte ch = data[offset];
+      //Single char utf character - 0xxxxxxx
+      if( 0 == ( ch & 0x80 ) )
+      {
+        if( ch == 0 )
+        {
+          throw invalidClassFileException( offset, index );
+        }
+        chars[size++] = (char)ch;
+        offset += 1;
+      }
+      //Double char utf character - 110x xxxx 10xx xxxx
+      else if( 0xC0 == ( ch & 0xE0 ) )
+      {
+        if( offset + 1 >= end )
+        {
+          throw invalidClassFileException( offset, index );
+        }
+        final byte ch2 = data[offset + 1];
+        if( 0x80 != ( ch2 & 0xC0 ) )
+        {
+          throw invalidClassFileException( offset, index );
+        }
+        chars[size++] = (char)( ( ( ch & 0x1F ) << 6 ) + ( ch2 & 0x3F ) );
+        offset += 2;
+      }
+      //Triple char utf character - 1110 xxxx 10xx xxxx 10xx xxxx
+      else if( 0xE0 == ( ch & 0xF0 ) )
+      {
+        if( offset + 2 >= end )
+        {
+          throw invalidClassFileException( offset, index );
+        }
+        final byte ch2 = data[offset + 1];
+        final byte ch3 = data[offset + 2];
+        if( 0x80 != ( ch2 & 0xC0 ) || 0x80 != ( ch3 & 0xC0 ) )
+        {
+          throw invalidClassFileException( offset, index );
+        }
+        chars[size++] =
+          (char)( ( ( ch & 0x0F ) << 12 ) + ( ( ch2 & 0x3F ) << 6 ) + ( ch3 & 0x3F ) );
+        offset += 3;
+      }
+      else
+      {
+        throw invalidClassFileException( offset, index );
+      }
+    }
+    return new String( chars, 0, size );
+  }
+
+  /**
+   * Create an ClassFormatError for invalid utf content.
+   *
+   * @param offset the offset.
+   * @param index  the index.
+   * @return the exception.
+   */
+  private static ClassFormatError invalidClassFileException( final int offset,
+                                                             final int index )
+  {
+    final String message =
+      "Constant pool entry " + index + " has invalid utf8 at " + offset;
+    return new ClassFormatError( message );
+  }
 }
--- src/test/java/org/realityforge/fade/TestIOUtil.java
+++ src/test/java/org/realityforge/fade/TestIOUtil.java
@@ -67,4 +67,117 @@ public class TestIOUtil
       assertEquals( "getMessage()", message, cfe.getMessage() );
     }
   }
+
+  public void test_parseUtfString()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF, (byte)0x8F, (byte)0xEF, (byte)0x8F, (byte)0x80};
+    final String value = IOUtil.parseUtfString( data, 0, 0, 6 );
+    assertEquals( "value.length", 3, value.length() );
+    assertEquals( "value.charAt( 0 )", 'a', value.charAt( 0 ) );
+    assertEquals( "value.charAt( 1 )", 975, value.charAt( 1 ) );
+    assertEquals( "value.charAt( 2 )", 62400, value.charAt( 2 ) );
+  }
+
+  public void test_parseUtfString_invalid_singlechar()
+    throws Exception
+  {
+    final byte[] data = new byte[]{0};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 1 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 0";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
+
+  public void test_parseUtfString_invalid_doublechar_tooshort()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 2 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 1";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
+
+  public void test_parseUtfString_invalid_doublechar()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF, (byte)0xFF};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 3 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 1";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
+
+  public void test_parseUtfString_invalid_triplechar_tooshort()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF, (byte)0x8F, (byte)0xEF, (byte)0x8F};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 5 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 3";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
+
+  public void test_parseUtfString_invalid_triplechar()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF, (byte)0x8F, (byte)0xEF, (byte)0x8F, (byte)0xF0};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 6 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 3";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
+
+  public void test_parseUtfString_psuedo_quadchar()
+    throws Exception
+  {
+    final byte[] data = new byte[]{'a', (byte)0xCF, (byte)0x8F, (byte)0xFF, (byte)0x8F, (byte)0x80, (byte)0x80};
+    try
+    {
+      IOUtil.parseUtfString( data, 0, 0, 7 );
+      fail( "Expected exception" );
+    }
+    catch( final ClassFormatError cfe )
+    {
+      final String message =
+        "Constant pool entry 0 has invalid utf8 at 3";
+      assertEquals( "getMessage()", message, cfe.getMessage() );
+    }
+  }
 }
